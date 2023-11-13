a70cc41f9a9f9fd995f2a73e20abfa8486ab0932
Tijn <67382928t-molder@users.noreply.github.com>
2023-11-13 13:45:48 +0100

    barebones frontend

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1); 
+?>
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible">
+    <title>Receptenboek</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+  </head>
+  <body>
+    <main>
+		<div class = "recipe">
+        	<h1>This is a recipe</h1> 
+		</div>
+		<div class = "ingredient-list">
+			<h2>these are the ingredients</h2>
+		</div>
+		<div class = "method">
+			<h2>this is the preparation method</h2>
+		</div>
+	</main>
+  </body>
+</html>
